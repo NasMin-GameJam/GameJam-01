a0fbaaea545b3b7951a2028066f95ee8a95448fb
Hakimin <myusoff2@dxc.com>
2020-09-09 10:31:15 +0800

    Player sign in info
    Saving sign in info into playerprefs

--- NasMi-GameJam-01/Assets/_Moi/03_Photon LobbyRoom/Script/Photon_Manager.cs
+++ NasMi-GameJam-01/Assets/_Moi/03_Photon LobbyRoom/Script/Photon_Manager.cs
@@ -4,61 +4,125 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using UnityEngine.UI;
 
 namespace moi.photonLobby
 {
     public class Photon_Manager : MonoBehaviourPunCallbacks
     {
+        [Header("UI")]
         public GameObject ConnectedPhotonPanel;
+        public Button btnConnectPhoton;
         public GameObject ErrorPanel;
 
         public TextMeshProUGUI errorText;
 
         public GameObject lobbyCanvas;
 
+        [Header("Room Options")]
+        public TMP_InputField roomName;
+        public Toggle isPublic;
+        public TMP_InputField maxPlayer;
+
+        int maxNum;
+
+
         string GameVersion = "0.0.0";
 
+        // Connect to Photon Server
         public void Connect()
         {
             PhotonNetwork.GameVersion = GameVersion;
             PhotonNetwork.ConnectUsingSettings();
             LogMessage("Connecting to server...");
+
+            // Just so the player doesn't spam the button
+            btnConnectPhoton.interactable = false;
         }
 
+        // If we're connected, do next step. In this case, open ConnectedPhotonPanel
         public override void OnConnectedToMaster()
         {
             LogMessage("Connected to server");
             ConnectedPhotonPanel.SetActive(true);
+
+            btnConnectPhoton.interactable = false;
         }
 
+        // If we're disconnected, what is the cause?
         public override void OnDisconnected(DisconnectCause cause)
         {
             ErrorPanel.SetActive(true);
             errorText.SetText(cause.ToString());
             LogMessage(cause.ToString());
+
+            btnConnectPhoton.interactable = true;
         }
 
+        // Custom debug message so we know it come from us.
         public void LogMessage(string message)
         {
             Debug.Log("Photon + PlayFab _Moi : " + message);
         }
 
+        // Usually, we don't need to join lobby if we join room
+        // Unless if we do search for empty room
+        // CS:Condition Zero use search room
+        // CS:GO use CreateOrJoinRandom room
         public override void OnJoinedLobby()
         {
             LogMessage("Joined Lobby");
         }
 
+        public override void OnLeftLobby()
+        {
+            LogMessage("Left Lobby");
+        }
+
         public void CreateRoom()
         {
-            PhotonNetwork.JoinOrCreateRoom("test",new RoomOptions { IsOpen = true }, TypedLobby.Default);
+            validateRoomOptions();
+
+            int randomRoomName = Random.Range(0, 10000);
+
+            PhotonNetwork.JoinOrCreateRoom(""+ randomRoomName ,new RoomOptions { IsOpen = isPublic, IsVisible = true, MaxPlayers = (byte)maxNum }, TypedLobby.Default);
         }
 
         public override void OnJoinedRoom()
         {
             lobbyCanvas.SetActive(false);
 
-            LogMessage("Joined Room");
+            LogMessage("Joined Room. " + roomName.text);
             //PhotonNetwork.Instantiate("player", Vector3.zero, Quaternion.identity, 0);
+        }
+
+        public void validateRoomOptions () {
+            maxNum = int.Parse(maxPlayer.text);
+            if(maxNum > 10)
+            {
+                maxNum = 10;
+                maxPlayer.text = "10";
+            }else if(maxNum < 0)
+            {
+                maxNum = 0;
+                maxPlayer.text = "0";
+            }
+        }
+
+        public override void OnCreateRoomFailed(short returnCode, string message)
+        {
+            LogMessage("Tried to create new but failed");
+            LogMessage(message);
+        }
+
+        public void OnCancelButtonClicked()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            base.OnPlayerEnteredRoom(newPlayer);
         }
     }
 }
---
+++ NasMi-GameJam-01/Assets/_Moi/03_Photon LobbyRoom/Script/Player_SignIn_Info.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace moi.photonLobby
+{
+    public class Player_SignIn_Info : MonoBehaviour
+    {
+        public TMP_InputField SignIn;
+        public TMP_InputField Password;
+
+        const string SignInInfo = "SignInInfo";
+        const string PasswordInfo = "PasswordInfo";
+
+        void Start()
+        {
+            if (PlayerPrefs.HasKey(SignInInfo))
+            {
+                SignIn.text = PlayerPrefs.GetString(SignInInfo);
+                Password.text = PlayerPrefs.GetString(PasswordInfo);
+
+                Debug.Log("Found sign in info");
+            }
+            else
+            {
+                Debug.Log("No sign info registered yet");
+            }
+        }
+
+        public void SaveInfo()
+        {
+            if (PlayerPrefs.HasKey(SignInInfo))
+            {
+                string tempSignIn = PlayerPrefs.GetString(SignInInfo);
+                if(SignIn.text != tempSignIn)
+                {
+                    PlayerPrefs.SetString(SignInInfo, SignIn.text);
+                    PlayerPrefs.SetString(PasswordInfo, Password.text);
+
+                    Debug.Log("Saved sign in info");
+                }
+            }
+            else
+            {
+                Debug.Log("No sign info registered yet. Saving one");
+                PlayerPrefs.SetString(SignInInfo, SignIn.text);
+                PlayerPrefs.SetString(PasswordInfo, Password.text);
+            }
+        }
+    }
+}
