44966dd03f379093ea7c7ff50dcce432610251bf
nasry05 <nasry05@users.noreply.github.com>
2020-09-27 18:44:02 +0700

    Sign up and Login function
    Created Signup, Login functions linked to the UI which I duplicated scene from the Main

---
+++ NasMi-GameJam-01/Assets/Gamejam Main/03_Scripts/Login Page/PlayFabLogin.cs
@@ -0,0 +1,96 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+    
+{
+    private string userEmail;
+    private string userPassword;
+    private string username;
+    private string userID;
+
+    public GameObject PanelLogin;
+
+    public void Start()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        {
+            
+            PlayFabSettings.staticSettings.TitleId = "E42DC";
+        }
+        //var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        //PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+
+        if (PlayerPrefs.HasKey("Email"))
+        {
+            userEmail = PlayerPrefs.GetString("EMAIL");
+            userPassword = PlayerPrefs.GetString("PASSWORD");
+
+            var request = new LoginWithEmailAddressRequest { Email = userEmail, Password = userPassword };
+            PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+        }
+    }
+
+    private void OnBecameVisible()
+    {
+        if (PlayerPrefs.HasKey("Email"))
+        {
+            userEmail = PlayerPrefs.GetString("EMAIL");
+            userPassword = PlayerPrefs.GetString("PASSWORD");
+
+            var request = new LoginWithEmailAddressRequest { Email = userEmail, Password = userPassword };
+            PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+        }
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        Debug.Log("Resut : " + result);
+        PanelLogin.SetActive(false);
+    }
+
+    private void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        //Debug.Log("Resut : " + result);
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+        PanelLogin.SetActive(false);
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        var registerRequest = new RegisterPlayFabUserRequest { Email = userEmail, Password = userPassword, Username = username };
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, OnRegisterSuccess, OnRegisterFailure);
+    }
+
+    private void OnRegisterFailure(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    public void GetUserEmail(string emailInput)
+    {
+        userEmail = emailInput;
+    }
+
+    public void GetUserPassword(string passwordInput)
+    {
+        userPassword = passwordInput;
+    }
+
+    public void GetUsername (string usernameInput)
+    {
+        username = usernameInput;
+    }
+
+    public void OnTapLogin()
+    {
+        var request = new LoginWithEmailAddressRequest { Email = userEmail, Password = userPassword };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+    }
+}
